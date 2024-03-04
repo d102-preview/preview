ea3c6406b3b3610f198bdeab485b4d54cc283789
이승현 <sh980812@naver.com>
2024-03-04 17:05:11 +0900

    [FE] setting #2: 프로젝트 초기 세팅
    - tailwind
    - zustand
    - TanStack Query v5
    - Axios
    - mock-service-worker
    - storybook

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+if (import.meta.env.VITE_NODE_ENV === 'develop') {
+  const { server } = await import('./mocks/browser.ts');
+  server.start();
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
