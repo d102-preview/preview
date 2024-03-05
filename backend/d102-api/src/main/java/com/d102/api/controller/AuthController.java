1703a034774863eb08fd525c11f4144d327e993b
Choi Hojo <ubermensch100326@gmail.com>
2024-03-05 21:28:28 +0900

    [BE] fix #1: 모듈 인식되지 않는 문제 수정

--- backend/d102-api/src/main/java/com/d102/api/controller/AuthController.java
+++ backend/d102-api/src/main/java/com/d102/api/controller/AuthController.java
@@ -1,7 +1,8 @@
 package com.d102.api.controller;
 
-//import com.d102.common.response.Response;
-//import org.springframework.web.bind.annotation.PostMapping;
+import com.d102.common.response.Response;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
-//    @PostMapping("/join")
-//    public Response join(){
-//        return new Response("message", "join success");
-//    }
+    @PostMapping("/join")
+    public Response join(){
+        return new Response("message", "join success");
+    }
 }
