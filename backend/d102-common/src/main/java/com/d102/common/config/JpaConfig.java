89accb62693dbd6d61fabeeae681329694c3b815
서준호 <twg0995@naver.com>
2024-03-06 17:58:05 +0900

    [BE] feat #16: 사용자 관련 엔티티 추가
    - 사용자 도메인 생성
    - 공통 시간 관리 클래스 생성
    - Jpa 설정 추가

---
+++ backend/d102-common/src/main/java/com/d102/common/config/JpaConfig.java
@@ -0,0 +1,26 @@
+package com.d102.common.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaAuditing
+@EntityScan("com.d102")
+@EnableJpaRepositories("com.d102")
+public class JpaConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
---
+++ backend/d102-common/src/main/java/com/d102/common/domain/BaseTime.java
@@ -0,0 +1,30 @@
+package com.d102.common.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTime {
+
+    @CreatedDate
+    @Column(name = "created_time")
+    private LocalDateTime createdTime;
+
+    @LastModifiedDate
+    @Column(name = "updated_time")
+    private LocalDateTime updatedTime;
+
+}
