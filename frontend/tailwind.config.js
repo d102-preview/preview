d86c4074ccf112461deea035067e3b05ef0ad872
이승현 <sh980812@naver.com>
2024-03-08 13:51:55 +0900

    [FE] setting: tailwind css 기본 색깔 설정

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        MAIN1: '#5A8AF2',
+        MAIN2: '#AEC5FF',
+        SUB: '#F8FAFF',
+        BLACK: '#404040',
+        GRAY: '#FAFAFA',
+        UNIMPORTANT_TEXT: '#A7A7A7',
+      },
+    },
   },
   plugins: [],
 };
