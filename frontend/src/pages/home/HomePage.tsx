abee93ccf87754a6ab32d808e099664469a8e5a2
이수화 <tnghk9611@naver.com>
2024-03-11 15:53:26 +0900

    [FE] feat #6: footer 메인페이지에 적용

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,9 +1,12 @@
+import Footer from '@/components/@common/Footer/Footer';
 import Header from '@/components/@common/Header/Header';
 
 const HomePage = () => {
   return (
     <div>
       <Header />
+      {/* @TODO: 페이지 내용 넣기 */}
+      <Footer />
     </div>
   );
 };
