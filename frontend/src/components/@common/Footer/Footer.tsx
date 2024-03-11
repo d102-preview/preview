0b53064adb4970b21714c3a7462866cea643ad58
이수화 <tnghk9611@naver.com>
2024-03-11 15:52:52 +0900

    [FE] feat #6: footer 컴포넌트 UI 구현

---
+++ frontend/src/components/@common/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <div className="w-full h-40 flex items-center pl-6 text-sm text-gray-600 bg-gray-100">
+      <div>
+        <span className="text-gray-600 font-semibold">서비스명</span>
+        <br />
+        Copyright © SSAFY. All rights reserved.
+        <br />
+        <br />
+        4반에 내린 단비 <span className="text-gray-300">•</span> 팀장 신주용
+        <br />
+        경북 구미시 3공단3로 302
+        <br />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
