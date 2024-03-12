72662a217aa51f9e2aba7ce3e6292b1525413d21
이승현 <sh980812@naver.com>
2024-03-12 09:54:34 +0900

    [FE] fix: service worker 빌드 오류 수정
    Top-level await 구문 에러로 인해 async-await 구문으로 수정

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,17 +4,25 @@ import App from './App.tsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-if (import.meta.env.VITE_NODE_ENV === 'develop') {
-  const { server } = await import('./mocks/browser.ts');
-  server.start();
+async function enableMocking() {
+  if (import.meta.env.VITE_NODE_ENV !== 'development') {
+    console.log('Sad');
+    return;
+  }
+  const { worker } = await import('./mocks/browser.ts');
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
 }
 
 const client = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={client}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </QueryClientProvider>,
-);
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <QueryClientProvider client={client}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </QueryClientProvider>,
+  );
+});
