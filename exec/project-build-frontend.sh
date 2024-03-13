5be1128a8c5bf1ea5d060b6effa19cf9473a5a07
Choi Hojo <ubermensch100326@gmail.com>
2024-03-13 13:50:57 +0900

    [INFRA] deploy #31: 프론트 빌드 스크립트 수정

--- exec/project-build-frontend.sh
+++ exec/project-build-frontend.sh
@@ -1,1 +1,1 @@
-cd ../frontend && rm -rf dist && rm -rf node_modules && rm -rf package-lock.json && chmod -R 755 . && cp ${FRONTEND_ENV} . && ls -al && npm install && npm run build
+cd ../frontend && rm -rf dist && chmod -R 755 . && cp ${FRONTEND_ENV} . && ls -al && npm install && npm run build
