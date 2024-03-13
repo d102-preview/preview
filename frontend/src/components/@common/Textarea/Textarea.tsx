a31cffd1693a8bababb8802d21f5485a735025d9
박단비 <silverwill39@gmail.com>
2024-03-14 00:10:16 +0900

    [FE] feat #27: 공통 컴포넌트 Textarea 구현

---
+++ frontend/src/components/@common/Textarea/Textarea.tsx
@@ -0,0 +1,89 @@
+import { TextareaHTMLAttributes, useState, useEffect, useRef } from 'react';
+
+interface ISubTextProps {
+  text: string;
+  type: 'success' | 'info' | 'error';
+}
+
+// textarea 태그 기본 props 상속
+interface ITextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  width?: string;
+  label?: string;
+  placeholder?: string;
+  borderType?: 'bottom' | 'all';
+  disabled?: boolean;
+  subText?: ISubTextProps;
+  maxLength?: number;
+}
+
+const Textarea = ({ width = 'w-full', label, borderType = 'all', subText, maxLength, ...props }: ITextareaProps) => {
+  // useState를 사용하여 text 상태를 관리
+  const [text, setText] = useState((props.defaultValue as string) || '');
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  // textarea의 높이를 조절하는 함수
+  const adjustHeight = () => {
+    if (!textareaRef.current) return;
+    textareaRef.current.style.height = 'auto'; // 높이를 auto로 설정하여 초기화
+    textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`; // scrollHeight 값을 이용해 높이 조절
+  };
+
+  // text 상태가 변화할 때마다 높이 조절
+  useEffect(() => {
+    adjustHeight();
+  }, [text]);
+
+  // 사용자 입력을 처리하고 상태를 업데이트하는 handleChange 함수
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const inputValue = event.target.value;
+    // maxLength가 정의되어 있고, 입력값의 길이가 maxLength를 초과하는 경우 입력값을 잘라냄
+    if (maxLength && inputValue.length > maxLength) {
+      setText(inputValue.slice(0, maxLength));
+    } else {
+      setText(inputValue);
+    }
+    adjustHeight(); // 입력 상태가 변화할 때마다 높이 조절
+  };
+
+  const setSubTextStyle = () => {
+    switch (subText?.type) {
+      case 'success':
+        return 'text-green-700';
+      case 'info':
+        return 'text-gray-800';
+      case 'error':
+        return 'text-red-500';
+      default:
+        return '';
+    }
+  };
+
+  return (
+    <div className="pb-5">
+      {label && <div className="pb-1.5 text-md">{label}</div>}
+      <div
+        className={`${width} ${borderType === 'all' ? 'border-2 rounded-lg' : 'border-b-2'} bg-white focus-within:border-MAIN1 overflow-hidden`}
+        style={{ padding: '0.75rem' }}
+      >
+        <textarea
+          {...props}
+          ref={textareaRef}
+          value={text}
+          onChange={handleChange}
+          className="w-full h-6 text-sm resize-none outline-none overflow-y-hidden"
+          maxLength={maxLength}
+        />
+      </div>
+      <div className="flex justify-between items-center mt-2">
+        {subText ? (
+          <div className={`text-xs ${setSubTextStyle()}`}>{subText.text}</div>
+        ) : (
+          <div className="flex-grow"></div> // subText가 없을 때 이 div가 남은 공간을 차지
+        )}
+        <div className="text-xs text-right">{`${text.length}${maxLength ? ` / ${maxLength}` : ''}자`}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Textarea;
--- frontend/src/pages/question/QuestionPage.tsx
+++ frontend/src/pages/question/QuestionPage.tsx
@@ -1,9 +1,72 @@
+// import Header from '@/components/@common/Header/Header';
+// const QuestionPage = () => {
+//   return (
+//     <>
+//       <Header />
+//       <div>질문 페이지 입니다</div>
+//     </>
+//   );
+// };
+
+// export default QuestionPage;
 import Header from '@/components/@common/Header/Header';
+import { useState } from 'react';
+import Accordian from '@/components/@common/Accordian/Accordian';
+import Textarea from '@/components/@common/Textarea/Textarea';
+import { MdOutlineExpandMore, MdOutlineExpandLess } from 'react-icons/md';
+
 const QuestionPage = () => {
+  // 각 아코디언의 열림 상태를 관리하기 위한 상태 변수
+  const [isOpenFirst, setIsOpenFirst] = useState(false);
+  const [isOpenSecond, setIsOpenSecond] = useState(false);
+  const [isOpenThird, setIsOpenThird] = useState(false);
+
+  // 토글 함수
+  const toggleFirst = () => setIsOpenFirst(!isOpenFirst);
+  const toggleSecond = () => setIsOpenSecond(!isOpenSecond);
+  const toggleThird = () => setIsOpenThird(!isOpenThird);
+
   return (
     <>
       <Header />
       <div>질문 페이지 입니다</div>
+
+      {/* 첫 번째 아코디언 */}
+      <Accordian titleContent="첫 번째 아코디언" children={<Textarea />} isOpen={isOpenFirst} onToggle={toggleFirst} />
+      {/* 두 번째 아코디언 */}
+      <Accordian
+        titleContent="두 번째 아코디언"
+        children={
+          <Textarea
+            placeholder="해당 질문에 대한 스크립트를 미리 작성해보세요!"
+            maxLength={10}
+            subText={{ text: '최대 10자까지 입력 가능합니다.', type: 'info' }}
+          />
+        }
+        isOpen={isOpenSecond}
+        onToggle={toggleSecond}
+        iconOpen={MdOutlineExpandLess}
+        iconClose={MdOutlineExpandMore}
+        iconOpenColor="#5A8AF2"
+        iconCloseColor="#5A8AF2"
+        backgroundColor="bg-[#F1F5FF]"
+        textColor="text-[#5A8AF2]"
+        textWeight="font-bold"
+      />
+      {/* 세 번째 아코디언 */}
+      <Accordian
+        titleContent="세 번째 아코디언"
+        children={<Textarea maxLength={30} label="스크립트" />}
+        isOpen={isOpenThird}
+        onToggle={toggleThird}
+        iconOpen={MdOutlineExpandLess}
+        iconClose={MdOutlineExpandMore}
+        iconOpenColor="#5A8AF2"
+        iconCloseColor="#5A8AF2"
+        backgroundColor="bg-[#F1F5FF]"
+        textColor="text-[#5A8AF2]"
+        textWeight="font-bold"
+      />
     </>
   );
 };
