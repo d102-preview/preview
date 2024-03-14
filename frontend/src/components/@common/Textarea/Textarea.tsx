a6407b7618beaadadaeea0b67592994b05dd8c56
박단비 <silverwill39@gmail.com>
2024-03-14 21:01:42 +0900

    [FE] feat #27: Textarea 컴포넌트의 스토리 작성

--- frontend/src/components/@common/Textarea/Textarea.tsx
+++ frontend/src/components/@common/Textarea/Textarea.tsx
@@ -10,13 +10,22 @@ interface ITextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   width?: string;
   label?: string;
   placeholder?: string;
-  borderType?: 'bottom' | 'all';
+  borderType?: 'none' | 'all';
   disabled?: boolean;
   subText?: ISubTextProps;
   maxLength?: number;
 }
 
-const Textarea = ({ width = 'w-full', label, borderType = 'all', subText, maxLength, ...props }: ITextareaProps) => {
+const Textarea = ({
+  width = 'w-full',
+  label,
+  placeholder,
+  borderType = 'all',
+  disabled,
+  subText,
+  maxLength,
+  ...props
+}: ITextareaProps) => {
   // useState를 사용하여 text 상태를 관리
   const [text, setText] = useState((props.defaultValue as string) || '');
   const textareaRef = useRef<HTMLTextAreaElement>(null);
@@ -24,7 +33,7 @@ const Textarea = ({ width = 'w-full', label, borderType = 'all', subText, maxLen
   // textarea의 높이를 조절하는 함수
   const adjustHeight = () => {
     if (!textareaRef.current) return;
-    textareaRef.current.style.height = 'auto'; // 높이를 auto로 설정하여 초기화
+    textareaRef.current.style.height = 'auto'; // 높이를 auto로 설정하여 textarea의 현재 내용에 상관없이 높이를 초기화(scrollHeight를 정확하게 측정하기 위함)
     textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`; // scrollHeight 값을 이용해 높이 조절
   };
 
@@ -62,16 +71,19 @@ const Textarea = ({ width = 'w-full', label, borderType = 'all', subText, maxLen
     <div className="pb-5">
       {label && <div className="pb-1.5 text-md">{label}</div>}
       <div
-        className={`${width} ${borderType === 'all' ? 'border-2 rounded-lg' : 'border-b-2'} bg-white focus-within:border-MAIN1 overflow-hidden`}
+        className={`${width} ${borderType === 'all' ? 'border-2 rounded-lg' : 'border-none'} 
+        ${disabled ? 'bg-gray-100' : 'bg-white'}  focus-within:border-MAIN1 overflow-hidden`}
         style={{ padding: '0.75rem' }}
       >
         <textarea
           {...props}
           ref={textareaRef}
           value={text}
+          disabled={disabled}
           onChange={handleChange}
           className="w-full h-6 text-sm resize-none outline-none overflow-y-hidden"
           maxLength={maxLength}
+          placeholder={placeholder}
         />
       </div>
       <div className="flex justify-between items-center mt-2">
