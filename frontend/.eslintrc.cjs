c079a80f326e85e4bee6e8c8738843610cec57f5
이승현 <sh980812@naver.com>
2024-03-14 16:37:44 +0900

    [FE] setting #23: storybook 8.0.0 version upgrade
    - 7.6.17 -> 8.0.0 으로 버전 변경

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,16 +1,7 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
-    // Make sure it's always the last config, so it gets the chance to override other configs.
-    'eslint-config-prettier',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'eslint-config-prettier', 'plugin:storybook/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
