6472a3daa01b78715133f52b5578573213fffa78
Choi Hojo <ubermensch100326@gmail.com>
2024-03-16 00:26:47 +0900

    [INFRA] deploy #31: 백엔드 스웨거 의존성 복구

--- backend/d102-file/src/main/java/com/d102/file/controller/HealthController.java
+++ backend/d102-file/src/main/java/com/d102/file/controller/HealthController.java
@@ -1,18 +1,18 @@
-//package com.d102.file.controller;
-//
-//import com.d102.file.controller.docs.HealthControllerDocs;
-//import com.d102.common.response.Response;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RequestMapping("/hc")
-//@RestController
-//public class HealthController implements HealthControllerDocs {
-//
-//    @Override
-//    @GetMapping
-//    public Response healthCheck() {
-//        return new Response();
-//    }
-//}
+package com.d102.file.controller;
+
+import com.d102.file.controller.docs.HealthControllerDocs;
+import com.d102.common.response.Response;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/hc")
+@RestController
+public class HealthController implements HealthControllerDocs {
+
+    @Override
+    @GetMapping
+    public Response healthCheck() {
+        return new Response();
+    }
+}
