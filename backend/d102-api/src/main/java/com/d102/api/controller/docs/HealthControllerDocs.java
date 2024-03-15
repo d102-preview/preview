faba3c33c6f96a8b4df5173b1f1dded664e8b1ee
Choi Hojo <ubermensch100326@gmail.com>
2024-03-15 23:39:23 +0900

    [INFRA] deploy #31: 백엔드 서버 상태 확인 컨트롤러 추가

--- backend/d102-api/src/main/java/com/d102/api/controller/docs/HealthControllerDocs.java
+++ backend/d102-api/src/main/java/com/d102/api/controller/docs/HealthControllerDocs.java
@@ -1,6 +1,5 @@
 package com.d102.api.controller.docs;
 
-import com.d102.api.dto.request.EmailDto;
 import com.d102.common.response.Response;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -10,16 +9,16 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-@Tag(name = "1. 인증 관련 API", description = "JWT 토큰을 요구하지 않는 API (회원가입, 이메일 중복확인 등)")
-public interface AuthControllerDocs {
+@Tag(name = "0. 서버 상태 관련 API", description = "배포 시 서버의 상태를 확인하는 API")
+public interface HealthControllerDocs {
 
-    @Operation(summary = "이메일 중복확인", description = "이메일의 중복을 확인한다.")
+    @Operation(summary = "서버 상태 확인", description = "서버의 상태를 확인한다.")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "이메일 중복 여부 판단",
+            @ApiResponse(responseCode = "200", description = "서버 상태 확인 성공",
                     content = @Content(schemaProperties = {
                             @SchemaProperty(name = "result", schema = @Schema(defaultValue = "ok", description = "요청 성공")),
-                            @SchemaProperty(name = "isDuplicateEmail", schema = @Schema(defaultValue = "true", description = "중복으로 사용 불가")),
-                    }))
+                    })
+            )
     })
-    Response checkDuplicatedEmail(EmailDto emailDto);
+    Response healthCheck();
 }
---
+++ backend/d102-file/src/main/java/com/d102/file/controller/docs/HealthControllerDocs.java
@@ -0,0 +1,24 @@
+package com.d102.file.controller.docs;
+
+import com.d102.common.response.Response;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.SchemaProperty;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "0. 서버 상태 관련 API", description = "배포 시 서버의 상태를 확인하는 API")
+public interface HealthControllerDocs {
+
+    @Operation(summary = "서버 상태 확인", description = "서버의 상태를 확인한다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "서버 상태 확인 성공",
+                    content = @Content(schemaProperties = {
+                            @SchemaProperty(name = "result", schema = @Schema(defaultValue = "ok", description = "요청 성공")),
+                    })
+            )
+    })
+    Response healthCheck();
+}
