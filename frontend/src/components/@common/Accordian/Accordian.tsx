f467c07f39e7024519328b6c3e9fd28eaa3028e8
박단비 <silverwill39@gmail.com>
2024-03-16 13:23:17 +0900

    [FE] feat #27: Accordian 컴포넌트의 상태관리 추가

--- frontend/src/components/@common/Accordian/Accordian.tsx
+++ frontend/src/components/@common/Accordian/Accordian.tsx
@@ -1,11 +1,11 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { IconType } from 'react-icons';
 import { CgFileDocument } from 'react-icons/cg';
 
 interface IAccordianProps {
   titleContent: string | React.ReactNode;
   children: string | React.ReactNode;
-  isOpen: boolean;
+  defaultOpen?: boolean; // 초기 상태를 설정하기 위한 프로퍼티
   iconOpen?: IconType;
   iconClose?: IconType;
   iconOpenColor?: string;
@@ -17,13 +17,13 @@ interface IAccordianProps {
   textSize?: string;
   textWeight?: string;
   width?: string;
-  onToggle: () => void;
+  onToggle?: () => void; // 사용자 정의 토글 함수(추가적인 액션)가 있는 경우
 }
 
 const Accordian = ({
   titleContent,
   children,
-  isOpen,
+  defaultOpen = false,
   iconOpen: IconOpen = CgFileDocument,
   iconClose: IconClose = CgFileDocument,
   iconOpenColor = '#5A8AF2',
@@ -37,7 +37,19 @@ const Accordian = ({
   width = 'w-10/12',
   onToggle,
 }: IAccordianProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(defaultOpen);
   const wrapperRef = useRef<HTMLDivElement>(null);
+
+  // isOpen 상태를 토글하는 함수
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+
+    // 사용자 지정 토글 함수가 있으면, 상태 변경 후에 호출
+    // 컴포넌트의 상태 변경 로직에 영향을 주지 않고, 추가적인 작업을 수행하는 용도로 사용됨
+    if (onToggle) {
+      onToggle();
+    }
+  };
 
   // isOpen 상태에 따라 동적으로 스타일 변경
   const wrapperStyle = isOpen ? { height: 'auto' } : { height: '0px' };
@@ -48,7 +60,7 @@ const Accordian = ({
     <div
       className={`m-4 cursor-pointer ${width} ${backgroundColor} ${borderStyle} ${borderColor} ${borderRadius} ${shadow}`}
     >
-      <div className={`p-3 mx-3 flex justify-between`} onClick={onToggle}>
+      <div className={`p-3 mx-3 flex justify-between`} onClick={toggleOpen}>
         <div className={` ${textColor} ${textSize} ${textWeight}`}>{titleContent}</div>
         <div>
           {isOpen ? <IconOpen size="25" color={iconOpenColor} /> : <IconClose size="25" color={iconCloseColor} />}
