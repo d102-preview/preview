5e6e12fa46021c21b50d37d27f09062bb3d63788
Danbi <silverwill39@gmail.com>
2024-03-18 01:29:02 +0900

    [FE] feat #29: 스크립트 수정 및 저장 기능 추가
    - 스크립트 수정 기능 추가
    - 스크립트 저장 기능 추가
    - Textarea 공통 컴포넌트 수정

--- frontend/src/pages/question/QuestionPage.tsx
+++ frontend/src/pages/question/QuestionPage.tsx
@@ -2,6 +2,7 @@ import Header from '@/components/@common/Header/Header';
 import Accordian from '@/components/@common/Accordian/Accordian';
 import Textarea from '@/components/@common/Textarea/Textarea';
 import Keywords from '@/components/question/Keywords';
+import Script from '@/components/question/Script';
 import { MdOutlineExpandMore, MdOutlineExpandLess } from 'react-icons/md';
 
 const QuestionPage = () => {
@@ -19,7 +20,7 @@ const QuestionPage = () => {
           titleContent="첫 번째 아코디언"
           children={
             <>
-              <Textarea placeholder="해당 질문에 대한 스크립트를 미리 작성해보세요!" maxLength={500} />
+              <Script initialScript="단점이 없는 게 장점입니다." maxLength={20} />
               <Keywords />
             </>
           }
@@ -31,6 +32,7 @@ const QuestionPage = () => {
           children={
             <>
               <Textarea
+                value="하이이"
                 placeholder="해당 질문에 대한 스크립트를 미리 작성해보세요!"
                 maxLength={10}
                 subText={{ text: '최대 10자까지 입력 가능합니다.', type: 'info' }}
@@ -50,7 +52,7 @@ const QuestionPage = () => {
         {/* 세 번째 아코디언 */}
         <Accordian
           titleContent="세 번째 아코디언"
-          children={<Textarea maxLength={30} label="스크립트" />}
+          children={<Textarea value="" maxLength={30} label="스크립트" />}
           defaultOpen={true}
           iconOpen={MdOutlineExpandLess}
           iconClose={MdOutlineExpandMore}
