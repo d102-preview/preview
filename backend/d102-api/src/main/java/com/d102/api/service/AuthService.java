8b1cbbb0e5cf61336d7f825eff9d21d1ba8a9d9d
서준호 <twg0995@naver.com>
2024-03-18 17:07:52 +0900

    [BE] feat #50: 회원가입 API 추
    - 회원가입 API 구현
    - 스프링 시큐리티 설정 파일 추가
    - MapStruct mapper 의존성 추가
    - Cors 관련 origin 추가

--- backend/d102-api/src/main/java/com/d102/api/service/AuthService.java
+++ backend/d102-api/src/main/java/com/d102/api/service/AuthService.java
@@ -1,8 +1,13 @@
 package com.d102.api.service;
 
 import com.d102.api.dto.request.EmailDto;
+import com.d102.api.dto.request.JoinDto;
+import com.d102.api.dto.response.UserResponseDto;
 
 public interface AuthService {
 
     Boolean checkDuplicatedEmail(EmailDto emailDto);
+
+    UserResponseDto join(JoinDto joinDto);
+
 }
