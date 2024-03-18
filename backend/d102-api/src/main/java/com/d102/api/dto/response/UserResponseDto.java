074e4dd48350eb306812437d3f6fedae98707410
서준호 <twg0995@naver.com>
2024-03-18 17:07:52 +0900

    [BE] feat #50: 회원가입 API 추
    - 회원가입 API 구현
    - 스프링 시큐리티 설정 파일 추가
    - MapStruct mapper 의존성 추가
    - Cors 관련 origin 추가

---
+++ backend/d102-api/src/main/java/com/d102/api/dto/response/UserResponseDto.java
@@ -0,0 +1,14 @@
+package com.d102.api.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserResponseDto {
+
+    private String email;
+    private String profileImage;
+    private String name;
+
+}
