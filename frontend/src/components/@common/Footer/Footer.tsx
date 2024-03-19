eaa7ae0d24f031df535de6ad814aa81bf6119139
이수화 <tnghk9611@naver.com>
2024-03-19 09:07:46 +0900

    [FE] fix #6: Footer 문구 수정

--- frontend/src/components/@common/Footer/Footer.tsx
+++ frontend/src/components/@common/Footer/Footer.tsx
@@ -2,9 +2,9 @@ const Footer = () => {
   return (
     <div className="w-full h-40 flex items-center pl-6 text-sm text-gray-600 bg-gray-100">
       <div>
-        <span className="text-gray-600 font-semibold">서비스명</span>
+        <span className="text-gray-600 font-semibold">Preview</span>
         <br />
-        Copyright © SSAFY. All rights reserved.
+        © SSAFY 2024
         <br />
         <br />
         4반에 내린 단비 <span className="text-gray-300">•</span> 팀장 신주용
