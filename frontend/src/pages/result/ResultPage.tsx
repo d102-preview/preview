e369d0e41a3ccf6b4f959d41947a3d484b171381
Danbi <silverwill39@gmail.com>
2024-03-21 01:55:45 +0900

    [FE] feat #30: ResultItem UI 구현

--- frontend/src/pages/result/ResultPage.tsx
+++ frontend/src/pages/result/ResultPage.tsx
@@ -1,5 +1,46 @@
+import Header from '@/components/@common/Header/Header';
+import ResultItem from '@/components/result/ResultItem';
+import IMAGE1 from '@/assets/images/interview1.png';
+import { useState } from 'react';
+
 const ResultPage = () => {
-  return <div>결과 페이지 입니다</div>;
+  const [type, setType] = useState<'mock' | 'main'>('mock');
+
+  const buttonClass = (buttonType: 'mock' | 'main') =>
+    `text-lg text-${type === buttonType ? 'MAIN1' : 'UNIMPORTANT_TEXT'} font-semibold border-b-2 border-${type === buttonType ? 'MAIN1' : 'UNIMPORTANT_TEXT'}`;
+
+  return (
+    <>
+      <Header />
+      <main className="max-w-7xl mx-auto mt-7">
+        <div className="flex justify-between">
+          <div className="flex gap-2 text-2xl font-bold">
+            <h3 className="text-[#696969]">{type == 'mock' ? '모의 면접' : '실전 면접'} 분석 결과</h3>
+            <h3 className="text-MAIN1">13</h3>
+          </div>
+          <div>
+            <button className={`${buttonClass('main')} mr-5`} onClick={() => setType('main')}>
+              실전 면접
+            </button>
+            <button className={buttonClass('mock')} onClick={() => setType('mock')}>
+              모의 면접
+            </button>
+          </div>
+        </div>
+        <div className="flex">
+          <ResultItem
+            result="ok"
+            id={1}
+            imagePath={IMAGE1}
+            type="main"
+            date={new Date()}
+            question="지원자의 강점(장점)은 무엇입니까?"
+            time="01:12"
+          />
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default ResultPage;
