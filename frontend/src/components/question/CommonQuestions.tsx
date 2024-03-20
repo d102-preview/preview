c948c015287fff2a33a379d95abbc53a77da6fcb
Danbi <silverwill39@gmail.com>
2024-03-20 22:48:56 +0900

    [FE] feat #29: 이력서 선택 드롭다운 기능 추가

--- frontend/src/components/question/CommonQuestions.tsx
+++ frontend/src/components/question/CommonQuestions.tsx
@@ -9,12 +9,9 @@ interface QuestionsProps {
 const CommonQuestions = ({ questions, type }: QuestionsProps) => {
   return (
     <div>
-      <h3 className="text-lg">공통 면접 질문</h3>
-      <p className="text-UNIMPORTANT_TEXT">총 {questions.length}개의 질문이 있습니다.</p>
-      <QuestionsList
-        questions={questions}
-        type={type}
-      />
+      <h3 className="text-lg font-semibold">공통 면접 질문</h3>
+      <p className="text-UNIMPORTANT_TEXT pt-1">총 {questions.length}개의 질문이 있습니다.</p>
+      <QuestionsList questions={questions} type={type} />
     </div>
   );
 };
