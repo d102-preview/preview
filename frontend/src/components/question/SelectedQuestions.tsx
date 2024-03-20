3c966bf3374b9434a0bf4a605c0684c4866244ef
Danbi <silverwill39@gmail.com>
2024-03-20 09:11:31 +0900

    [FE] feat #29: questionStore 추가
    - questionStore 추가
    - 질문 페이지 내부 div 스크롤바 추가
    - #25 Accordian 컴포넌트 수정

--- frontend/src/components/question/SelectedQuestions.tsx
+++ frontend/src/components/question/SelectedQuestions.tsx
@@ -1,28 +1,19 @@
 import { useNavigate } from 'react-router-dom';
 import Button from '../@common/Button/Button';
-import QuestionItem from './QuestionItem';
 import { LuMinusCircle } from 'react-icons/lu';
+import questionStore from '@/stores/questionStore';
 
-interface IQuestionObj {
-  question: string;
-  id: number;
-}
-
-interface ISelectedQuestionsProps {
-  selectedQuestion: IQuestionObj[]; // Props 타입을 IQuestionObj 배열로 변경합니다.
-  onRemove: (id: number) => void;
-}
-
-const SelectedQuestions = ({ selectedQuestion, onRemove }: ISelectedQuestionsProps) => {
+const SelectedQuestions = () => {
+  const { selectedQuestions, removeQuestion, message } = questionStore();
   const navigate = useNavigate();
 
   return (
     <div className="flex flex-col justify-between ml-10 p-7 w-1/3 rounded-2xl shadow-lg">
       <div>
         <h3 className="text-xl font-semibold mb-1">선택된 질문</h3>
-        <p className="text-sm text-UNIMPORTANT_TEXT">총 {selectedQuestion.length}개의 질문이 선택되었습니다.</p>
+        <p className="text-sm text-UNIMPORTANT_TEXT">총 {selectedQuestions.length}개의 질문이 선택되었습니다.</p>
         <ul>
-          {selectedQuestion.map(questionObj => (
+          {selectedQuestions.map(questionObj => (
             <div
               key={questionObj.id}
               className={`m-4 p-3 cursor-pointer box-content drop-shadow-lg w-10/12 bg-white rounded-xl flex items-center`}
@@ -30,12 +21,13 @@ const SelectedQuestions = ({ selectedQuestion, onRemove }: ISelectedQuestionsPro
               <LuMinusCircle
                 size="35"
                 className="flex-shrink-0 mr-2 p-1 pl-0 text-MAIN1"
-                onClick={() => onRemove(questionObj.id)}
+                onClick={() => removeQuestion(questionObj.id)}
               />
               <div>{questionObj.question}</div>
             </div>
           ))}
         </ul>
+        {message && <p className="text-red-500 text-sm m-4">{message}</p>}
       </div>
 
       <div className="flex flex-end justify-end gap-2">
--- frontend/src/pages/question/QuestionPage.tsx
+++ frontend/src/pages/question/QuestionPage.tsx
@@ -1,28 +1,19 @@
+import questionStore from '@/stores/questionStore';
 import Header from '@/components/@common/Header/Header';
 import QuestionItem from '@/components/question/QuestionItem';
 import QuestionTab from '@/components/question/QuestionTab';
 import SelectedQuestions from '@/components/question/SelectedQuestions';
-import { useEffect, useState } from 'react';
 
-interface IQuestionObj {
-  question: string;
-  id: number;
-}
+const questions = [
+  { id: 1, question: '1분 자기소개 해주세요' },
+  { id: 2, question: '본인 성격의 장단점에 대해 말해주세요' },
+  { id: 3, question: '인생에서 가장 중요한 것은 무엇인가요?' },
+  { id: 4, question: '리더쉽을 발휘했던 경험에 대해서 말씀해주세요.' },
+  { id: 5, question: '동료와 친구들은 본인을 어떻게 생각하나요?' },
+];
 
 const QuestionPage = () => {
-  const [selected, setSelected] = useState<IQuestionObj[]>([]);
-
-  const addQuestion = (questionObj: IQuestionObj) => {
-    setSelected(prev => [...prev, questionObj]);
-  };
-
-  const removeQuestion = (id: number) => {
-    setSelected(prev => prev.filter(questionObj => questionObj.id !== id));
-  };
-
-  useEffect(() => {
-    console.log(selected);
-  }, [selected]);
+  const { selectedQuestions, addQuestion, removeQuestion } = questionStore();
 
   return (
     <>
@@ -32,40 +23,27 @@ const QuestionPage = () => {
           <h3 className="text-xl font-semibold mb-1">면접 질문 리스트</h3>
           <p className="text-sm text-[#B0B0B0]">면접 연습을 진행할 리스트를 생성해주세요</p>
         </div>
-        <div className="mt-3 flex h-5/6">
+        <div className="mt-3 flex h-5/6 overflow-auto">
           <div className="flex rounded-2xl shadow-lg bg-GRAY w-10/12">
             <QuestionTab />
-            <div className="p-7 w-11/12">
+            <div className="p-7 w-11/12 overflow-auto">
               <h3 className="text-lg">공통 면접 질문</h3>
               <p className="text-UNIMPORTANT_TEXT">총 0개의 질문이 있습니다.</p>
               <div className="pr-7">
-                <QuestionItem
-                  question="본인의 장점이 무엇인가요?"
-                  id={1}
-                  onAdd={() => addQuestion({ id: 1, question: '1분 자기소개 해주세요' })}
-                  onRemove={() => removeQuestion(1)}
-                />
-                <QuestionItem
-                  question="자바스크립트 ES6+의 새로운 기능들을 사용한 구체적인 예를 들어주세요."
-                  id={2}
-                  onAdd={() =>
-                    addQuestion({
-                      id: 2,
-                      question: '자바스크립트 ES6+의 새로운 기능들을 사용한 구체적인 예를 들어주세요.',
-                    })
-                  }
-                  onRemove={() => removeQuestion(2)}
-                />
-                <QuestionItem
-                  question="인생에서 가장 중요한 것은 무엇인가요?"
-                  id={3}
-                  onAdd={() => addQuestion({ id: 3, question: '인생에서 가장 중요한 것은 무엇인가요?' })}
-                  onRemove={() => removeQuestion(3)}
-                />
+                {questions.map(question => (
+                  <QuestionItem
+                    key={question.id}
+                    question={question.question}
+                    id={question.id}
+                    isSelected={selectedQuestions.some(q => q.id === question.id)}
+                    onAdd={addQuestion}
+                    onRemove={removeQuestion}
+                  />
+                ))}
               </div>
             </div>
           </div>
-          <SelectedQuestions selectedQuestion={selected} onRemove={removeQuestion} />
+          <SelectedQuestions />
         </div>
       </main>
     </>
