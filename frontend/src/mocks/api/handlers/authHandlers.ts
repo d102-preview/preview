226865e7354197aaeb096a5d2f165bca6cddd97e
이수화 <tnghk9611@naver.com>
2024-03-21 17:46:37 +0900

    [FE] feat #26: 이메일 중복 확인 MSW 구현

---
+++ frontend/src/mocks/api/handlers/authHandlers.ts
@@ -0,0 +1,18 @@
+import { HttpResponse, http } from 'msw';
+import { getIsEmailDuplicateRes } from '../data/auth';
+
+export const authHandlers = [
+  http.get('/api/auth/email', ({ request }) => {
+    const url = new URL(request.url);
+    const email = url.searchParams.get('email');
+
+    if (!email) {
+      return new HttpResponse(null, { status: 404 });
+    }
+
+    const success = HttpResponse.json(getIsEmailDuplicateRes, { status: 200 });
+    const error = HttpResponse.json(null, { status: 409 });
+
+    return error;
+  }),
+];
