ca1c051ab4a97aa55d222ddd30f4b801d360e465
이수화 <tnghk9611@naver.com>
2024-03-21 17:51:21 +0900

    [FE] feat #26: 이메일 중복 확인 api 통신 구현

---
+++ frontend/src/services/auth/api.ts
@@ -0,0 +1,10 @@
+import { IGetIsEmailDuplicateRes } from '@/types/auth';
+// import { axiosAuthInstance } from '@/apis/axiosInstance';
+import { APIResponse } from '@/types/model';
+import axios from 'axios';
+
+export const getIsEmailDuplicate = async (email: string): Promise<APIResponse<IGetIsEmailDuplicateRes>> => {
+  const res = await axios.get(`/api/auth/email?email=${email}`);
+  console.log(res.data);
+  return res.data;
+};
