8b21d3fd65960f00ff2dcc67ddd23d9b0644b5f9
이승현 <sh980812@naver.com>
2024-03-21 17:51:58 +0900

    [FE] style #75: 타입명 수정
    List -> item 으로 직관적이게 변경

--- frontend/src/types/model/index.ts
+++ frontend/src/types/model/index.ts
@@ -5,15 +5,15 @@ export interface APIResponse<T> {
 }
 
 // 면접 질문 리스트 타입
-export interface IQuestionList {
+export interface IQuestionItem {
   id: number;
   question: string;
   type: 'common' | 'resume';
-  keywordList: IKeywordList[] | null;
+  keywordList: IKeywordItem[] | null;
 }
 
 // 면접 키워드 리스트 타입
-export interface IKeywordList {
+export interface IKeywordItem {
   id: number;
   keyword: string;
 }
