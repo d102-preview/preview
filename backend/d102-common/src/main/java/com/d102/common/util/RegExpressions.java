7979df1a8c3c05bbf1176475717396209ab0d124
Choi Hojo <ubermensch100326@gmail.com>
2024-03-22 10:08:01 +0900

    [BE] refactor #93: 패키지명 단수로 통일 및 공백 컨벤션 통일

--- backend/d102-common/src/main/java/com/d102/common/util/RegExpressions.java
+++ backend/d102-common/src/main/java/com/d102/common/util/RegExpressions.java
@@ -1,4 +1,4 @@
-package com.d102.common.utils;
+package com.d102.common.util;
 
 public class RegExpressions {
     public static final String strongPassword = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=?\\{\\};':\"|,.<>/~`\\[\\]\\\\])[A-Za-z\\d!@#$%^&*()_+\\-=?\\{\\};':\"|,.<>/~`\\[\\]]{8,}$";
