b05b1d2224883fa6327567a32a43b8f4a6e86263
Choi Hojo <ubermensch100326@gmail.com>
2024-03-23 00:25:59 +0900

    [FE] fix: 실행 스크립트 오류 수정

--- exec/project-set-environment.sh
+++ exec/project-set-environment.sh
@@ -1,5 +1,1 @@
-ORIGINAL_DIR=$(pwd)
-cd ../backend/d102-api/src/main/resources/ && chmod -R 755 . && cp ${PROD_YML} .
-
-cd ${ORIGINAL_DIR}
-cd ../backend/d102-file/src/main/resources/ && chmod -R 755 . && cp ${PROD_YML} .
+cd ../frontend && rm -rf dist && chmod -R 755 . && cp ${PROD_ENV} .
