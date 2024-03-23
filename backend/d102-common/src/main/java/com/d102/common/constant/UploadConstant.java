160b8f3234ff1bde119aaf4c110432db4715ab0b
Choi Hojo <ubermensch100326@gmail.com>
2024-03-23 21:01:29 +0900

    [BE] feat #96: 도커 컨테이너 기본 경로 테스트

--- backend/d102-common/src/main/java/com/d102/common/constant/UploadConstant.java
+++ backend/d102-common/src/main/java/com/d102/common/constant/UploadConstant.java
@@ -3,5 +3,6 @@ package com.d102.common.constant;
 public class UploadConstant {
 
     public static final String PROFILE_IMAGE = "profile";
+//    public static final String path = System.getProperty("user.dir") +
 
 }
