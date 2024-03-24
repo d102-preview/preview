70c26f72aa1a17992ea5fdd51cab9f5bcf1e7bcf
서준호 <twg0995@naver.com>
2024-03-24 13:18:36 +0900

    [BE] feat #104: 인증관련 상수 분리

--- backend/d102-common/src/main/java/com/d102/common/constant/AuthConstant.java
+++ backend/d102-common/src/main/java/com/d102/common/constant/AuthConstant.java
@@ -3,5 +3,12 @@ package com.d102.common.constant;
 public class AuthConstant {
 
     public static final String USER = "user";
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String AUTHORIZATION_PREFIX = "Bearer ";
+    public static final String AUTHORITIES_KEY = "auth";
+    public static final String REFRESH_TOKEN = "refreshToken";
+    public static final int SECOND_TO_MILLISECOND = 1000;
+    public static final int MILLISECOND_TO_SECOND = 1000;
+    public static final int TOKEN_BEGIN_IDX = 7;
 
 }
