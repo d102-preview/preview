9f98623d547af14a0bb3cdf75c4b8e1c9da44580
Danbi <silverwill39@gmail.com>
2024-03-24 17:29:12 +0900

    [FE] #fix #30: 타입 오류 수정

--- frontend/src/utils/formatDateTime.ts
+++ frontend/src/utils/formatDateTime.ts
@@ -1,7 +1,8 @@
 // 면접 시간 fomat 함수 - ex) 2024. 03. 24. 오후 05:05
-export const formatInterviewTime = (dateTimeString: string): string => {
+export const formatInterviewTime = (dateTimeString: string | Date): string => {
   const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: '2-digit', day: '2-digit' };
-  return new Date(dateTimeString).toLocaleDateString('ko-KR', options);
+  const date = typeof dateTimeString === 'string' ? new Date(dateTimeString) : dateTimeString;
+  return date.toLocaleDateString('ko-KR', options);
 };
 
 // 면접 세트 시간 fomat 함수 - ex) 2024. 03. 24.
