039d4a5607279c2be46d61ff81911474d340010b
서준호 <twg0995@naver.com>
2024-03-24 17:25:31 +0900

    [BE] feat #105: 회원삭제 API 구현
    - 회원삭제 API 구현
    - 로그인시 삭제된 회원 로그인 안되게 처리

--- backend/d102-common/src/main/java/com/d102/common/repository/UserRepository.java
+++ backend/d102-common/src/main/java/com/d102/common/repository/UserRepository.java
@@ -11,4 +11,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Transactional
     Optional<User> findByEmail(String email);
 
+    @Transactional
+    Optional<User> findByEmailAndDeletedTimeIsNull(String email);
+
 }
