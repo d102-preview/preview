d8a7bf919699a8c9d1139ddea1058fca3c56d45f
cheesecat47 <cheesecat47@gmail.com>
2024-03-25 14:04:02 +0900

    [BE] feat #33: 파라미터 유효성 검사, 커스텀 에러 핸들러 추가
    - 분석 요청 파라미터 중 `analysis_id`는 db의 레코드 아이디이므로 1
      이상이어야 함.
    - 해당 변수에 대해 유효성 검사를 추가하고, 기본 에러 메시지 대신 커스텀
      에서 메시지를 응답하도록 핸들러 추가.

--- api-ai/main.py
+++ api-ai/main.py
@@ -1,5 +1,7 @@
+from common.exception import custom_value_error_handler
 from core.settings import settings
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
 from loguru import logger
 from routes import analysis_router
 
@@ -7,4 +9,8 @@ logger.info("Run server")
 
 app = FastAPI()
 
+# Add routers(controllers)
 app.include_router(analysis_router, prefix=settings.API_V1_STR)
+
+# Add custom exception handlers
+app.add_exception_handler(RequestValidationError, custom_value_error_handler)
