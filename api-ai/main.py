1be94fff86c6563533f39d81fdcb17771f3db647
cheesecat47 <cheesecat47@gmail.com>
2024-03-27 00:39:50 +0900

    [BE] feat #126: 로그 메시지 형식 수정
    - fastapi 서버는 기본적으로 uvicorn을 활용해 실행됨. uvicorn 로그를
      loguru를 활용해 출력하고 싶었으나, 그렇게 하는 방법은 없는 것 같음.
    - 대신, uvicorn의 로그 형식을 loguru 로그 형식과 일치하도록 수정.

--- api-ai/main.py
+++ api-ai/main.py
@@ -1,11 +1,11 @@
+import uvicorn
 from common.exception import custom_validation_error_handler
+from core.logger import load_config
 from core.settings import settings
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 from loguru import logger
 from routes import analysis_router
-
-logger.info("Run server")
 
 app = FastAPI()
 
@@ -14,3 +14,8 @@ app.include_router(analysis_router, prefix=settings.API_V1_STR)
 
 # Add custom exception handlers
 app.add_exception_handler(RequestValidationError, custom_validation_error_handler)
+
+if __name__ == "__main__":
+    logger.info("Run server")
+
+    uvicorn.run("main:app", port=8000, reload=True, log_config=load_config())
