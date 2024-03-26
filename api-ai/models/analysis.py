70a491db1db7f7952095f0a24ba25f1b674fcc1a
cheesecat47 <cheesecat47@gmail.com>
2024-03-26 16:21:29 +0900

    [BE] feat #33: 서비스 로직 작성, DB 조회 구현

--- api-ai/models/analysis.py
+++ api-ai/models/analysis.py
@@ -1,13 +1,30 @@
+from datetime import datetime
+
+import sqlmodel
 from models.common import CommonResponse
 from pydantic import BaseModel, Field
+from sqlmodel import SQLModel
 
 
 class AnalysisRequest(BaseModel):
     """분석 요청 파라미터"""
 
     analysis_id: int = Field(description="분석 아이디", ge=1)
-    file_path: str = Field(description="분석할 영상 파일 경로")
 
 
 class AnalysisResponse(CommonResponse):
     """분석 응답"""
+
+
+class Analysis(SQLModel, table=True):
+    id: int = sqlmodel.Field(primary_key=True)
+    user_id: int = sqlmodel.Field(foreign_key="user.id", nullable=False)
+    type: str = sqlmodel.Field(max_length=16)
+    question: str = sqlmodel.Field(max_length=512)
+    video_path: str = sqlmodel.Field(max_length=512)
+    thumbnail_path: str = sqlmodel.Field(max_length=512)
+    keyword: str = sqlmodel.Field(max_length=128)
+    set_start_time: datetime
+    analysis_req_time: datetime
+    analysis_start_time: datetime | None = sqlmodel.Field(default=None, nullable=True)
+    analysis_end_time: datetime | None = sqlmodel.Field(default=None, nullable=True)
---
+++ api-ai/service/analysis.py
@@ -0,0 +1,31 @@
+from core.db import engine
+from fastapi import HTTPException, status
+from loguru import logger
+from models.analysis import Analysis
+from sqlmodel import Session, select
+
+
+def create_task(analysis_id: int) -> None:
+    """
+    파라미터로 받은 `analysis_id`를 사용해 해당하는 분석 요청을 분석 작업 큐에 등록한다.
+
+    Args:
+        analysis_id (int): DB `analysis` 테이블의 레코드 아이디.
+
+    Raises:
+        HTTPException: `analysis_id`에 해당하는 레코드가 없는 경우.
+    """
+    logger.info("Create task")
+
+    with Session(engine) as session:
+        stmt = select(Analysis).where(Analysis.id == analysis_id)
+        result = session.exec(stmt).first()
+
+        if result is None:
+            msg = f"No analysis record of id #{analysis_id}. Please check again."
+            logger.error(msg)
+
+            # TODO: Replace HTTPException with custom exception
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=msg)
+
+    return
