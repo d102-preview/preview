111650a34f28f98262891dd4541dc33e2c0c0211
cheesecat47 <cheesecat47@gmail.com>
2024-03-26 12:16:54 +0900

    [BE] setting #33: DB 연결 설졍

--- api-ai/core/settings.py
+++ api-ai/core/settings.py
@@ -1,8 +1,13 @@
-from pydantic_settings import BaseSettings
+from pydantic import Field, MariaDBDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    API_V1_STR: str = "/ai"
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    API_V1_STR: str = Field("/ai")
+
+    DATABASE_DSN: MariaDBDsn
 
 
 settings = Settings()
