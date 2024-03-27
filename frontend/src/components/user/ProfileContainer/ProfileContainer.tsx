caee918b996e63e16019965a23f2a4447ab4f8a4
suhwa <tnghk9611@naver.com>
2024-03-28 04:10:40 +0900

    [FE] feat #144: 회원정보 조회 후 마이페이지 적용

--- frontend/src/components/user/ProfileContainer/ProfileContainer.tsx
+++ frontend/src/components/user/ProfileContainer/ProfileContainer.tsx
@@ -1,13 +1,23 @@
+import { useUser } from '@/hooks/user/useUser';
 import EtcButton from '../EtcButton/EtcButton';
 import MyInfo from '../MyInfo/MyInfo';
 import Resume from '../Resume/Resume';
 
 const ProfileContainer = () => {
+  const { useGetUser } = useUser();
+  const { data, isLoading, isError } = useGetUser();
+
+  console.log(data);
+
+  if (isLoading) return <div>로딩중</div>;
+  if (!data) return <div>로딩</div>;
+  if (isError) return <div>에러</div>;
+
   return (
     <div className="w-[40%] min-w-[400px] mx-auto py-10 animate-showUp">
       <h1 className="text-2xl py-6">프로필</h1>
-      <MyInfo />
-      <Resume />
+      <MyInfo user={data?.data?.user} />
+      <Resume resume={data?.data?.user?.resumeList} />
       <EtcButton />
     </div>
   );
