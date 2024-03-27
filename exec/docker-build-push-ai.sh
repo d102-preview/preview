8a45179a8e2d114165593ec9c5f532e58f921d7e
cheesecat47 <cheesecat47@gmail.com>
2024-03-27 23:57:19 +0900

    [BE] fix #136: Recover copy-docker-compose-env.sh

--- exec/docker-build-push-ai.sh
+++ exec/docker-build-push-ai.sh
@@ -5,8 +5,6 @@ export REGISTRY=localhost:5000
 
 export IMG_NAME=api-ai
 
-chmod -R 755 "./deploy-$IMG_NAME"
-
 docker build -t "$REGISTRY/$IMG_NAME:$TAG" ../api-ai
 
 docker push "$REGISTRY/$IMG_NAME:$TAG"
