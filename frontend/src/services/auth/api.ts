14c38ecbd332623ddde25abc90ed2de83defd821
suhwa <tnghk9611@naver.com>
2024-03-27 23:23:31 +0900

    [FE] feat #150: refresh token을 localstorage에 저장

--- frontend/src/services/auth/api.ts
+++ frontend/src/services/auth/api.ts
@@ -1,26 +1,25 @@
+import { axiosCommonInstance } from '@/apis/axiosInstance';
 import {
   IGetEmailDuplicateRes,
   IPostEmailCertificationRes,
   IPostEmailVerifyReq,
   IPostEmailVerifyRes,
+  IPostJoinReq,
+  IPostJoinRes,
+  IPostLoginReq,
   IPostLoginRes,
 } from '@/types/auth';
 import { APIResponse } from '@/types/model';
-import axios from 'axios';
-import { IPostJoinReq, IPostJoinRes, IPostLoginReq } from './../../types/auth';
 
 export const getIsEmailDuplicate = async (email: string): Promise<APIResponse<IGetEmailDuplicateRes>> => {
-  const res = await axios.get(`/api/email?email=${email}`);
-  console.log('1)', res.data);
+  const res = await axiosCommonInstance.get(`/api/email?email=${email}`);
   return res.data;
 };
 
 export const postEmailCertification = async (email: string): Promise<APIResponse<IPostEmailCertificationRes>> => {
-  const res = await axios.post('/api/email', {
+  const res = await axiosCommonInstance.post('/api/email', {
     email: email,
   });
-
-  console.log('1)', res.data);
   return res.data;
 };
 
@@ -28,30 +27,31 @@ export const postVerifyEmail = async ({
   email,
   authorizationCode,
 }: IPostEmailVerifyReq): Promise<APIResponse<IPostEmailVerifyRes>> => {
-  const res = await axios.post('/api/email/verify', {
+  const res = await axiosCommonInstance.post('/api/email/verify', {
     email: email,
     authorizationCode: authorizationCode,
   });
-
-  console.log('1)', res.data);
   return res.data;
 };
 
 export const postSignup = async ({ email, password, name }: IPostJoinReq): Promise<APIResponse<IPostJoinRes>> => {
-  const res = await axios.post('/api/auth/join', {
+  const res = await axiosCommonInstance.post('/api/auth/join', {
     email: email,
     password: password,
     name: name,
   });
-
   return res.data;
 };
 
 export const postLogin = async ({ email, password }: IPostLoginReq): Promise<APIResponse<IPostLoginRes>> => {
-  const res = await axios.post('/api/auth/login', {
+  const res = await axiosCommonInstance.post('/api/auth/login', {
     email: email,
     password: password,
   });
+
+  if (res.status === 200 && res.headers.authorization) {
+    localStorage.setItem('PREVIEW_REFRESH_TOKEN', res.headers.authorization);
+  }
 
   return res.data;
 };
