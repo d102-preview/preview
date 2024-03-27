8b40e2add7b2519f5612fd964b709f80e0d22ee3
Danbi <silverwill39@gmail.com>
2024-03-27 21:30:58 +0900

    [FE] fix #111: 질문 상세 조회 API 통신 수정
    - useQuery를  useMutation 훅으로 변경

--- frontend/src/services/question/api.ts
+++ frontend/src/services/question/api.ts
@@ -7,13 +7,13 @@ export const getQuestionList = async (type: questionType): Promise<APIResponse<I
   // @TODO: 무한 스크롤로 변경하기
   const params = { page: 0, size: 10 };
   const { data } = await axiosCommonInstance.get(`/api/${type}/question/list`, { params });
-  console.log('질문 리스트', data);
+  console.log('1) 질문 리스트', data);
   return data;
 };
 
 // @TODO: 헤더에 토큰 보내기
 export const getQuestion = async (type: questionType, questionId: number): Promise<APIResponse<IQuestionRes>> => {
   const { data } = await axiosCommonInstance.get(`/api/${type}/question?${type}QuestionId=${questionId}`);
-  console.log('질문 상세', data);
+  console.log('1) 질문 상세', data);
   return data;
 };
