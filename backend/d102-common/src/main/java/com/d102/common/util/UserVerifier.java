50dec6c612d0272e59cb5406e4cf754f7533a070
서준호 <twg0995@naver.com>
2024-03-27 13:38:27 +0900

    [BE] refactor #123: 공통질문 valid 추가 및 UserVerifier 변수명 변경

--- backend/d102-common/src/main/java/com/d102/common/util/UserVerifier.java
+++ backend/d102-common/src/main/java/com/d102/common/util/UserVerifier.java
@@ -6,8 +6,8 @@ import org.apache.commons.lang3.StringUtils;
 
 public class UserVerifier {
 
-    public static void checkLoginUserAndResourceUser(String loginEmail, String resumeEmail) {
-        if (!StringUtils.equals(loginEmail, resumeEmail)) {
+    public static void checkLoginUserAndResourceUser(String loginEmail, String resourceEmail) {
+        if (!StringUtils.equals(loginEmail, resourceEmail)) {
             throw new NotFoundException(ExceptionType.NotVerifyUserException);
         }
     }
