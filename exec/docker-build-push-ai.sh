eb968ea1ddcc5f80c5e696d6ccf8171000650ebd
cheesecat47 <cheesecat47@gmail.com>
2024-03-28 00:18:46 +0900

    [BE] fix #136: Restore

--- exec/docker-build-push-ai.sh
+++ exec/docker-build-push-ai.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+export TAG=0.0.1
+export REGISTRY=localhost:5000
+
+export IMG_NAME=api-ai
+
 docker build -t "$REGISTRY/$IMG_NAME:$TAG" ../api-ai
 
 docker push "$REGISTRY/$IMG_NAME:$TAG"
