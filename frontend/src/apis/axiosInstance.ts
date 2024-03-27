3556cb95e0d76165a78f38bb0307afb4b9038b99
suhwa <tnghk9611@naver.com>
2024-03-28 03:00:18 +0900

    [FE] feat #144: msw 작동 상황 아닐때 기본 url 수정

--- frontend/src/apis/axiosInstance.ts
+++ frontend/src/apis/axiosInstance.ts
@@ -2,11 +2,9 @@ import axios from 'axios';
 import setAuthorization from './interceptors';
 
 const axiosRequestConfig = {
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: import.meta.env.VITE_NODE_ENV === 'development' ? '' : import.meta.env.VITE_BASE_URL,
 };
 
 export const axiosCommonInstance = axios.create(axiosRequestConfig);
 
-export const axiosAuthInstance = axios.create(axiosRequestConfig);
-
-axiosAuthInstance.interceptors.request.use(setAuthorization);
+axiosCommonInstance.interceptors.request.use(setAuthorization);
