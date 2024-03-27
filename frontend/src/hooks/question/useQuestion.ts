c200e328a15991245e4878cd6bde4822d762df19
Danbi <silverwill39@gmail.com>
2024-03-27 15:31:19 +0900

    [FE] feat #111: 질문 리스트 조회 API 연결

--- frontend/src/hooks/question/useQuestion.ts
+++ frontend/src/hooks/question/useQuestion.ts
@@ -1,12 +1,13 @@
 import { getQuestionList, getQuestion } from '@/services/question/api';
 import { useQuery } from '@tanstack/react-query';
+import { questionType } from '@/types/question';
 
 export const useQuestion = () => {
-  const useGetQuestionList = (type: 'common' | 'resume') => {
+  const useGetQuestionList = (type: questionType) => {
     return useQuery({ queryKey: ['questionList', type], queryFn: () => getQuestionList(type) });
   };
 
-  const useGetQuestion = (type: 'common' | 'resume', questionId: number) => {
+  const useGetQuestion = (type: questionType, questionId: number) => {
     return useQuery({
       queryKey: [`${type}Question`, questionId],
       queryFn: () => getQuestion(type, questionId),
