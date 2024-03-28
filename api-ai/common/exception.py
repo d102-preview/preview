9dfcfde3ae47bc4541f540d0e85f11e5a9e37eb8
cheesecat47 <cheesecat47@gmail.com>
2024-03-28 14:02:06 +0900

    [BE] feat #152: 영상 파일이 존재하지 않는 경우 예외 처리

--- api-ai/common/exception.py
+++ api-ai/common/exception.py
@@ -9,7 +9,7 @@ from models.common import Code, CommonResponse, Status
 async def custom_validation_error_handler(
     request: Request, exc: RequestValidationError
 ):
-    logger.debug(request.scope["route"])
+    logger.error(request.scope["route"])
 
     return JSONResponse(
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
@@ -19,6 +19,21 @@ async def custom_validation_error_handler(
                 code=Code.INVALID,
                 message=exc.errors()[0]["msg"],
                 data=exc.errors()[0],
+            )
+        ),
+    )
+
+
+async def custom_file_not_fount_error_handler(request: Request, exc: FileNotFoundError):
+    logger.error(request.scope["route"])
+
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content=jsonable_encoder(
+            CommonResponse(
+                result=Status.FAIL,
+                code=Code.INVALID,
+                message=str(exc),
             )
         ),
     )
