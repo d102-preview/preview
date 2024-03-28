8dc39b796bbc0ec39dc412091598b7d6292c7044
cheesecat47 <cheesecat47@gmail.com>
2024-03-28 12:48:49 +0900

    [BE] feat #152: DB 연결 의존성 주입

--- api-ai/service/analysis.py
+++ api-ai/service/analysis.py
@@ -6,10 +6,10 @@ from fastapi import HTTPException, status
 from loguru import logger
 from models.analysis import Analysis
 from pytz import timezone
-from sqlmodel import Session, select
+from sqlmodel import select
 
 
-def create_task(analysis_id: int) -> None:
+def create_task(analysis_id: int, session: SessionDep) -> None:
     """
     파라미터로 받은 `analysis_id`를 사용해 해당하는 분석 요청을 분석 작업 큐에 등록한다.
 
@@ -21,20 +21,19 @@ def create_task(analysis_id: int) -> None:
     """
     logger.info("Create task")
 
-    with Session(engine) as session:
-        stmt = select(Analysis).where(Analysis.id == analysis_id)
-        result = session.exec(stmt).first()
+    stmt = select(Analysis).where(Analysis.id == analysis_id)
+    record = session.exec(stmt).one_or_none()
 
-        if result is None:
-            msg = f"No analysis record of id #{analysis_id}. Please check again."
-            logger.error(msg)
+    if record is None:
+        msg = f"No analysis record of id #{analysis_id}. Please check again."
+        logger.error(msg)
 
-            # TODO: Replace HTTPException with custom exception
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=msg)
+        # TODO: Replace HTTPException with custom exception
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=msg)
 
-        # Set analysis start time(`analysis_start_time`) as current time
-        result.analysis_start_time = datetime.now(tz=timezone(settings.TZ))
-        session.add(result)
-        session.commit()
+    # Set analysis start time(`analysis_start_time`) as current time
+    record.analysis_start_time = datetime.now(tz=timezone(settings.TZ))
+    session.add(record)
+    session.commit()
 
     return
