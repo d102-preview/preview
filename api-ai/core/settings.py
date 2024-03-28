540e3e99e2e2ceca0dd33a795c3d3716637c7fcf
cheesecat47 <cheesecat47@gmail.com>
2024-03-28 12:46:41 +0900

    [BE] fix #152: 환경변수 업데이트

--- api-ai/core/settings.py
+++ api-ai/core/settings.py
@@ -1,17 +1,25 @@
-from pydantic import Field, MariaDBDsn
+from pydantic import DirectoryPath, Field, MariaDBDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
-    API_V1_STR: str = Field("/ai")
+    DEBUG: bool = False
 
-    DB_HOST: str
-    DB_PORT: int
-    DATABASE_DSN: MariaDBDsn
+    # Server variables
+    API_V1_STR: str = Field("/ai")
+    DATA_HOME: DirectoryPath
 
     TZ: str = "Asia/Seoul"
+
+    # Database variables
+    DATABASE_DSN: MariaDBDsn
+
+    # Build variables
+    REGISTRY: str
+    IMG_NAME: str
+    TAG: str
 
 
 settings = Settings()
