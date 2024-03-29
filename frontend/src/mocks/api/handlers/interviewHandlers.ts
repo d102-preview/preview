24f93c7031a093710d7a935ac33c75ce81845fa4
이승현 <sh980812@naver.com>
2024-03-29 17:59:09 +0900

    [FE] feat #181: 면접 분석 요청 MSW 구현

--- frontend/src/mocks/api/handlers/interviewHandlers.ts
+++ frontend/src/mocks/api/handlers/interviewHandlers.ts
@@ -1,8 +1,12 @@
 import { HttpResponse, http } from 'msw';
-import { interviewQuestionRes } from '../data/interview';
+import { interviewAnalyzeRes, interviewQuestionRes } from '../data/interview';
 
 export const interviewHandlers = [
   http.get('/api/interview/main', () => {
     return HttpResponse.json(interviewQuestionRes, { status: 201 });
+  }),
+
+  http.post('file/upload/video', () => {
+    return HttpResponse.json(interviewAnalyzeRes, { status: 201 });
   }),
 ];
