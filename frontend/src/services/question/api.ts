17998e3a0a226691ef1ca48f2a42a2ebaeaa0b54
Danbi <silverwill39@gmail.com>
2024-03-29 17:06:24 +0900

    [FE] feat #176: 공통 질문 스크립트 작성 API 통신
    - 스크립트 작성 API 통신
    - 스크립트가 비어있을 시 '수정'이 아닌 '작성'으로 수정
    - 스크립트 제한 글자 수 constant 파일에 정의

--- frontend/src/services/question/api.ts
+++ frontend/src/services/question/api.ts
@@ -1,4 +1,4 @@
-import { IQuestionListRes, IQuestionRes, questionType } from '@/types/question';
+import { IQuestionListRes, IQuestionRes, questionType, IScriptInfo, IScriptRes } from '@/types/question';
 import { APIResponse } from '@/types/model';
 import { axiosAuthInstance, axiosCommonInstance } from '@/apis/axiosInstance';
 
@@ -11,5 +11,11 @@ export const getQuestionList = async (type: questionType): Promise<APIResponse<I
 
 export const getQuestion = async (type: questionType, questionId: number): Promise<APIResponse<IQuestionRes>> => {
   const res = await axiosAuthInstance.get(`/api/${type}/question/${questionId}`);
+  return res.data;
+};
+
+export const postScrtip = async ({ type, questionId, script }: IScriptInfo): Promise<APIResponse<IScriptRes>> => {
+  const res = await axiosAuthInstance.post(`/api/${type}/question/script/${questionId}`, script);
+  console.log('스크립트 작성', res);
   return res.data;
 };
