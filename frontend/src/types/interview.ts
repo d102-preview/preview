4ef0373b0036dce138f3061be928007a073d79c7
이승현 <sh980812@naver.com>
2024-03-30 19:25:22 +0900

    [FE] feat #181: 면접 분석 요청 type 수정

--- frontend/src/types/interview.ts
+++ frontend/src/types/interview.ts
@@ -1,4 +1,4 @@
-import { IQuestionItem, interviewType } from './model';
+import { IKeywordItem, IQuestionItem, interviewType } from './model';
 
 export interface IInterviewQuestionRes {
   questionList: IQuestionItem[];
@@ -8,8 +8,7 @@ export interface IInterviewAnalyzeRes {
   type: interviewType;
   question: string;
   answer: string;
-  script: string;
-  keyword: string[];
   skip: boolean;
   setStartTime: string;
+  keywordList: IKeywordItem[];
 }
