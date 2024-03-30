06e5d4e08ce60755117521faa2db4bb8341cbf27
이승현 <sh980812@naver.com>
2024-03-30 19:30:15 +0900

    [FE] feat #181: 면접 분석 요청 판단 여부를 관리할 skip 구현
    영상 길이가 10초 이내라면 skip

--- frontend/src/components/record/InterviewQuestion.tsx
+++ frontend/src/components/record/InterviewQuestion.tsx
@@ -7,6 +7,7 @@ interface IInterviewQuestionProps {
   question: IQuestionItem;
   status: recordStatusType;
   setStatus: Dispatch<SetStateAction<recordStatusType>>;
+  setSkip: Dispatch<SetStateAction<boolean>>;
   handleStopRecording: () => void;
 }
 
@@ -15,6 +16,7 @@ const InterviewQuestion = ({
   question,
   status,
   setStatus,
+  setSkip,
   handleStopRecording,
 }: IInterviewQuestionProps) => {
   const [count, setCount] = useState<number>(timerSetting);
@@ -40,6 +42,7 @@ const InterviewQuestion = ({
       }
 
       return () => {
+        timerSetting - count < 10 ? setSkip(true) : setSkip(false);
         clearInterval(timer);
       };
     }
