6e1ceb47acf2cb4a78c8b7ef3b70d9c0b8206722
이승현 <sh980812@naver.com>
2024-03-30 19:25:22 +0900

    [FE] feat #181: 면접 분석 요청 type 수정

--- frontend/src/types/model/index.ts
+++ frontend/src/types/model/index.ts
@@ -11,10 +11,9 @@ export interface APISimpleResponse {
 
 // 면접 질문 리스트 타입
 export interface IQuestionItem {
-  id: number;
   question: string;
   type: interviewType;
-  keywordList: IKeywordItem[] | null;
+  keywordList: IKeywordItem[];
 }
 
 export type interviewType = 'common' | 'resume' | 'followup';
@@ -27,7 +26,6 @@ export interface IScriptItem {
 
 // 면접 키워드 리스트 타입
 export interface IKeywordItem {
-  id: number;
   keyword: string;
 }
 
