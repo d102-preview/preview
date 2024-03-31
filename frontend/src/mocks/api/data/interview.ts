d90563886616585525b973173f6c75fcae9195fa
이승현 <sh980812@naver.com>
2024-03-31 17:22:06 +0900

    [FE] feat #204: 꼬리 질문 생성 MSW 구현

--- frontend/src/mocks/api/data/interview.ts
+++ frontend/src/mocks/api/data/interview.ts
@@ -59,3 +59,7 @@ export const interviewQuestionRes: APIResponse<IInterviewQuestionRes> = {
 export const interviewAnalyzeRes: APISimpleResponse = {
   result: 'ok',
 };
+
+export const interviewFollowupRes: APISimpleResponse = {
+  result: 'ok',
+};
