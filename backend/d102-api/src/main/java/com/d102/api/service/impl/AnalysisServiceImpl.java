0a567fd6519426e4b4143ad63043f498a828ad6c
서준호 <twg0995@naver.com>
2024-03-31 18:46:21 +0900

    [BE] feat #196: 분석 목록 조회 API 수정

--- backend/d102-api/src/main/java/com/d102/api/service/impl/AnalysisServiceImpl.java
+++ backend/d102-api/src/main/java/com/d102/api/service/impl/AnalysisServiceImpl.java
@@ -4,8 +4,6 @@ import com.d102.api.dto.AnalysisDto;
 import com.d102.api.mapper.AnalysisMapper;
 import com.d102.api.service.AnalysisService;
 import com.d102.common.constant.InterviewType;
-import com.d102.common.domain.jpa.Interview;
-import com.d102.common.repository.jpa.AnalysisRepository;
 import com.d102.common.repository.jpa.InterviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -13,27 +11,17 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @RequiredArgsConstructor
 @Service
 public class AnalysisServiceImpl implements AnalysisService {
 
-    private final AnalysisRepository analysisRepository;
     private final InterviewRepository interviewRepository;
     private final AnalysisMapper analysisMapper;
 
     @Transactional(readOnly = true)
     public Page<AnalysisDto.ListResponse> getList(String type, Pageable pageable) {
-        Page<Interview> analysisList = interviewRepository.findByType(InterviewType.valueOf(type), pageable);
-        System.out.println("analysisList = " + analysisList.getContent());
-
-        // print analysisList
-        for (Interview interview : analysisList) {
-            System.out.println("interview = " + interview.getId());
-        }
-
-        return null;
+        return interviewRepository.
+                findByType(InterviewType.valueOf(type), pageable).map(analysisMapper::toAnalysisListResponseDto);
     }
 
 }
--- backend/d102-common/src/main/java/com/d102/common/constant/InterviewType.java
+++ backend/d102-common/src/main/java/com/d102/common/constant/InterviewType.java
@@ -1,7 +1,5 @@
 package com.d102.common.constant;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-
 public enum InterviewType {
 
     main,
