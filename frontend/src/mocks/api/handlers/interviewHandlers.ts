5a843f8ebfd5b16ef5f3b3013d1f80f59bf3b506
이승현 <sh980812@naver.com>
2024-03-31 20:32:57 +0900

    [FE] feat #204: 꼬리 질문 생성 api 구현

--- frontend/src/mocks/api/handlers/interviewHandlers.ts
+++ frontend/src/mocks/api/handlers/interviewHandlers.ts
@@ -1,4 +1,4 @@
-import { HttpResponse, http } from 'msw';
+import { HttpResponse, delay, http } from 'msw';
 import { interviewAnalyzeRes, interviewFollowupRes, interviewQuestionRes } from '../data/interview';
 
 export const interviewHandlers = [
@@ -10,8 +10,11 @@ export const interviewHandlers = [
     return HttpResponse.json(interviewAnalyzeRes, { status: 201 });
   }),
 
-  http.post('api/followup/question', ({ request }) => {
+  http.post('api/followup/question', async ({ request }) => {
     console.log('꼬리질문 생성 request : ' + request);
+
+    await delay(4000);
+
     return HttpResponse.json(interviewFollowupRes, { status: 201 });
   }),
 ];
