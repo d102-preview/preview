4209304e33457faca0bf3a2f1a9bff17377cc386
cheesecat47 <cheesecat47@gmail.com>
2024-03-31 17:18:27 +0900

    [BE] setting #202: Redis 연결 설정

--- api-ai/core/db.py
+++ api-ai/core/db.py
@@ -1,4 +1,16 @@
+import redis
 from core.settings import settings
 from sqlmodel import create_engine
 
 engine = create_engine(str(settings.DATABASE_DSN), echo=True)
+
+# Connect to Redis
+redisConn = redis.Redis(
+    connection_pool=redis.ConnectionPool(
+        host=settings.REDIS_HOST,
+        port=settings.REDIS_PORT,
+        db=settings.REDIS_DB,
+        password=settings.REDIS_PASSWORD,
+        max_connections=settings.REDIS_MAX_CONN,
+    )
+)
