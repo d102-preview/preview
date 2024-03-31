ae41e786bfe34e843058ec7ee3ef861015bd9a74
suhwa <tnghk9611@naver.com>
2024-04-01 00:35:32 +0900

    [FE] feat #172: HomeContent 컴포넌트 추가

---
+++ frontend/src/components/home/HomeContent.tsx
@@ -0,0 +1,19 @@
+import FifthSection from './FifthSection';
+import FirstSection from './FirstSection';
+import FourthSection from './FourthSection';
+import SecondSection from './SecondSection';
+import ThirdSection from './ThirdSection';
+
+const HomeContent = () => {
+  return (
+    <>
+      <FirstSection />
+      <SecondSection />
+      <ThirdSection />
+      <FourthSection />
+      <FifthSection />
+    </>
+  );
+};
+
+export default HomeContent;
