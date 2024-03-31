98d0261c00b866fd7ebcf67ea56d6988b2da4ce0
이승현 <sh980812@naver.com>
2024-03-31 21:35:21 +0900

    [FE] feat #208: 면접 세트 생성 및 아이디 조회 type 구현

--- frontend/src/types/interview.ts
+++ frontend/src/types/interview.ts
@@ -39,3 +39,18 @@ export interface IInterviewFollowupRes {
 export interface IFollowUpQuestion {
   question: string;
 }
+
+// 면접 세트 생성 및 아이디 조회 response
+export interface IInterviewSetRes {
+  interview: IInterviewSeId;
+}
+
+// 면접 세트 생성 및 아이디 조회 request
+export interface IInterviewSetReq {
+  type: 'main' | 'mock';
+  startTime: string;
+}
+
+export interface IInterviewSeId {
+  id: number;
+}
