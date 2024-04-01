30ecb82f7f18bdd2f84b54d9373f1b234731fd25
Choi Hojo <ubermensch100326@gmail.com>
2024-04-01 23:25:59 +0900

    [BE] feat #225: 영상 섬네일 다운로드 구현

--- backend/d102-common/src/main/java/com/d102/common/config/SecurityConfig.java
+++ backend/d102-common/src/main/java/com/d102/common/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
             "/health/**",
             /* profile */
             "/download/profile/**",
+            "/download/thumbnail/**",
     };
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
