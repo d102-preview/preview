d8379fc245d4ed0f4a188d66f776b2033ebe6977
이수화 <tnghk9611@naver.com>
2024-04-03 13:32:05 +0900

    [FE] fix: 인터셉터 수정

--- frontend/src/apis/interceptors.ts
+++ frontend/src/apis/interceptors.ts
@@ -1,15 +1,16 @@
 import { InternalAxiosRequestConfig } from 'axios';
-import { useNavigate } from 'react-router-dom';
 
 const setAuthorization = (config: InternalAxiosRequestConfig) => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const accessToken = localStorage.getItem('PREVIEW_ACCESS_TOKEN');
 
   if (accessToken) {
     config.headers.Authorization = `Bearer ${accessToken}`;
-  } else {
-    navigate('/login');
   }
+
+  // else {
+  //   navigate('/login');
+  // }
 
   return config;
 };
