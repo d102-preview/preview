9a88d198a86a2e9a854a4bb6f976cbb16d071fe7
Danbi <silverwill39@gmail.com>
2024-04-03 16:30:53 +0900

    [FE] fix #248: 질문 상세조회 로직 수정

--- frontend/src/components/question/QuestionItem.tsx
+++ frontend/src/components/question/QuestionItem.tsx
@@ -22,8 +22,9 @@ const QuestionItem = ({ question, id, isSelected, onAdd, onRemove, type }: IQues
   const location = useLocation();
   const isQuestionPage = location.pathname === '/question';
 
+  const [isOpen, setIsOpen] = useState(false);
   const { useGetQuestion } = useQuestion();
-  const { mutate: getQuestion, data } = useGetQuestion({ type, questionId: id });
+  const { data, refetch } = useGetQuestion({ type, questionId: id, isEnabled: isOpen });
 
   const { isLogin } = userStore();
 
@@ -32,7 +33,6 @@ const QuestionItem = ({ question, id, isSelected, onAdd, onRemove, type }: IQues
       Toast.info('스크립트 및 키워드 작성 기능을 이용하려면 로그인이 필요합니다.');
       return;
     }
-    getQuestion();
   };
 
   const script = data?.data?.questionDetail?.script?.script || '';
@@ -40,8 +40,15 @@ const QuestionItem = ({ question, id, isSelected, onAdd, onRemove, type }: IQues
 
   const [plusClick, setPlusClick] = useState<boolean>(false);
   const handlePlusClick = () => {
-    getQuestion();
     setPlusClick(true);
+  };
+
+  // 아코디언 토글 핸들러
+  const handleAccordionToggle = () => {
+    if (!isOpen) {
+      refetch(); // 아코디언이 닫혀있는 경우만 refetch 호출
+    }
+    setIsOpen(!isOpen); // 이후 상태를 반전
   };
 
   useEffect(() => {
@@ -63,6 +70,7 @@ const QuestionItem = ({ question, id, isSelected, onAdd, onRemove, type }: IQues
         hasIcons={isQuestionPage}
         textSize="text-base"
         textWeight="font-medium"
+        onToggle={handleAccordionToggle}
       >
         <Script initialScript={script} id={id} type={type} />
         <Keywords initialKeywords={keywords} id={id} type={type} />
