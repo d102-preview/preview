67c16676ac6deadb02747e132fb68640e80c0e71
Choi Hojo <ubermensch100326@gmail.com>
2024-04-05 16:03:47 +0900

    [BE] feat #260: 외부 API 호출 실패 시 재시도 로직 추가

--- backend/d102-api/src/main/java/com/d102/api/service/impl/FollowUpQuestionServiceImpl.java
+++ backend/d102-api/src/main/java/com/d102/api/service/impl/FollowUpQuestionServiceImpl.java
@@ -2,6 +2,7 @@ package com.d102.api.service.impl;
 
 import com.d102.api.dto.FollowUpQuestionDto;
 import com.d102.api.service.FollowUpQuestionService;
+import com.d102.common.constant.TaskConstant;
 import com.d102.common.exception.ExceptionType;
 import com.d102.common.exception.custom.InvalidException;
 import com.d102.common.util.OpenAiApi;
@@ -17,12 +18,29 @@ public class FollowUpQuestionServiceImpl implements FollowUpQuestionService {
 
     public FollowUpQuestionDto.Response get(FollowUpQuestionDto.Request requestDto) {
         OpenAiApi.Response response = null;
-        try {
-            response = openAiApi.generateFollowUpQuestion(requestDto.getQuestion(), requestDto.getAnswer());
-        } catch (RestClientException e) {
-            throw new InvalidException(ExceptionType.OpenAiApiException);
+        String question = null;
+
+        int retryCount = 0;
+        boolean isRetry = true;
+        while (isRetry && retryCount < TaskConstant.MAX_RETRY) {
+            try {
+                response = openAiApi.generateFollowUpQuestion(requestDto.getQuestion(), requestDto.getAnswer());
+                question = response.getChoices().getFirst().getMessage().getContent();
+                isRetry = false;
+                Thread.sleep(TaskConstant.RETRY_INTERVAL);
+            } catch (RestClientException e) {
+                retryCount++;
+                if (retryCount >= TaskConstant.MAX_RETRY) {
+                    throw new InvalidException(ExceptionType.OpenAiApiException);
+                }
+            } catch (Exception e) {
+                retryCount++;
+                if (retryCount >= TaskConstant.MAX_RETRY) {
+                    throw new InvalidException(ExceptionType.UnknownException);
+                }
+            }
         }
-        String question = response.getChoices().getFirst().getMessage().getContent();
+
         return FollowUpQuestionDto.Response.builder()
                 .question(question).build();
     }
