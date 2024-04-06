5b50a6a9c1f8131bdd74e6dedae143525ebe4c4d
Choi Hojo <ubermensch100326@gmail.com>
2024-04-07 02:48:27 +0900

    [BE] feat: 이력서 기반 질문 생성에서 텍스트 추출 방식 추가

--- backend/d102-common/src/main/java/com/d102/common/service/impl/SseServiceImpl.java
+++ backend/d102-common/src/main/java/com/d102/common/service/impl/SseServiceImpl.java
@@ -9,7 +9,6 @@ import com.d102.common.util.SecurityHelper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
