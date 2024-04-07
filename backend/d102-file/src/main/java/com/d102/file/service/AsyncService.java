778646c18136e7f74aaf16e553078e8345787669
Choi Hojo <ubermensch100326@gmail.com>
2024-04-07 21:15:30 +0900

    [BE] refactor: AsyncServiceImpl 메서드명 형식 통일 및 디버깅 로그 제거

--- backend/d102-file/src/main/java/com/d102/file/service/AsyncService.java
+++ backend/d102-file/src/main/java/com/d102/file/service/AsyncService.java
@@ -2,7 +2,7 @@ package com.d102.file.service;
 
 public interface AsyncService {
 
-    void generateAndSaveQuestionList(Long resumeId, String email);
+    void generateQuestionListByText(Long resumeId, String email);
 
     void analyzeVideo(Long analysisId);
 
