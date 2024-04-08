a33f766c16e608511e0a6941be82c189c373b74d
Danbi <silverwill39@gmail.com>
2024-04-08 14:06:16 +0900

    [FE] fix : 면접 시작 이미지 변경

--- frontend/src/components/interview/InterviewStartItem.tsx
+++ frontend/src/components/interview/InterviewStartItem.tsx
@@ -1,19 +1,27 @@
+import Lottie, { Options as LottieOptions } from 'react-lottie';
+
 interface IInterviewStartItemProps {
   tittle: string;
   subTitle: string;
-  imgSrc: string;
+  animationOptions: LottieOptions;
   advantageList: string[];
   children: React.ReactNode;
 }
 
-const InterviewStartItem = ({ tittle, subTitle, imgSrc, advantageList, children }: IInterviewStartItemProps) => {
+const InterviewStartItem = ({
+  tittle,
+  subTitle,
+  animationOptions,
+  advantageList,
+  children,
+}: IInterviewStartItemProps) => {
   return (
     <div className="w-80 flex flex-col gap-5 text-center">
       <div className="font-bold">
         <p className="text-sm text-BLACK">{subTitle}</p>
         <h2 className="text-6xl py-3">{tittle}</h2>
       </div>
-      <img src={imgSrc} className="w-56 mx-auto"></img>
+      <Lottie options={animationOptions} height={200} width={250} />
       <ul className="flex flex-col gap-1 text-sm text-left list-disc px-5 pb-3 ">
         {advantageList.map((item, index) => {
           return <li key={index}>{item}</li>;
