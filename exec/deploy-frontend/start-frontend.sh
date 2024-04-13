d30d727e1ee35ca9496b558b10b3577fe24007ae
Choi Hojo <ubermensch100326@gmail.com>
2024-04-14 03:01:45 +0900

    [INFRA] deploy: 프론트엔드 배포 테스트

--- exec/deploy-frontend/start-frontend.sh
+++ exec/deploy-frontend/start-frontend.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export REGISTRY=j10d102.p.ssafy.io
-#docker login $REGISTRY -u $USERNAME -p $PASSWORD
+docker login $REGISTRY -u $USERNAME -p $PASSWORD
 
 docker compose pull
 docker compose up -d
-docker system prune -f
+docker system prune -f  
